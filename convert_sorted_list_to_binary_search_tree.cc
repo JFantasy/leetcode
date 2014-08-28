921b2f4736fbe7cc5c287bfbcaa877d32dcfdbb9
jfantasy <jfantasy90@gmail.com>
2014-08-28 10:08:36 +0800

    update convert_sorted_list_to_binary_search_tree from O(nlogn) to O(n)

--- convert_sorted_list_to_binary_search_tree.cc
+++ convert_sorted_list_to_binary_search_tree.cc
@@ -17,20 +17,25 @@
  */
 class Solution {
   public:
-    TreeNode *build(ListNode *low, ListNode *upp) {
-      if (!low || low == upp) return NULL;
-      int length = 0;
-      ListNode *current;
-      for (current = low; current != upp; current = current->next) ++ length;
-      int mid = length >> 1;
-      current = low;
-      for (int i = 0; i < mid; ++ i, current = current->next);
-      TreeNode *res = new TreeNode(current->val);
-      res->left = build(low, current);
-      res->right = build(current->next, upp);
-      return res;
+    TreeNode *build(int low, int upp, ListNode **head, TreeNode *current) {
+      int mid = (low + upp) >> 1;
+      if (low < mid) {
+        current->left = new TreeNode(0);
+        build(low, mid - 1, head, current->left);
+      }
+      current->val = (*head)->val;
+      *head = (*head)->next;
+      if (mid < upp) {
+        current->right = new TreeNode(0);
+        build(mid + 1, upp, head, current->right);
+      }
     }
     TreeNode *sortedListToBST(ListNode *head) {
-      return build(head, NULL);
+      int len = 0;
+      for (ListNode *current = head; current; current = current->next) ++ len;
+      if (!len) return NULL;
+      TreeNode *res = new TreeNode(0);
+      build(1, len, &head, res);
+      return res;
     }
 };
