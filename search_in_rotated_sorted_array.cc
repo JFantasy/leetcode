85f08268871647c5d4f718330d1981d4b728364e
jfantasy <jfantasy90@gmail.com>
2014-08-28 13:50:29 +0800

    update search_in_rotated_sorted_array more simple

--- search_in_rotated_sorted_array.cc
+++ search_in_rotated_sorted_array.cc
@@ -1,5 +1,26 @@
 class Solution {
   public:
+    int search(int A[], int n, int target) {
+      int low = 0, upp = n - 1;
+      while (low <= upp) {
+        int mid = low + (upp - low) / 2;
+        if (A[mid] == target) return mid;
+        if (low < mid && A[low] < A[mid]) {
+          if (A[low] <= target && target <= A[mid - 1]) upp = mid - 1;
+          else low = mid + 1;
+        } else {
+          if (A[mid + 1] <= target && target <= A[upp]) low = mid + 1;
+          else upp = mid - 1;
+        }
+      }
+      return -1;
+    }
+};
+
+//------------------
+
+class Solution {
+  public:
     int find(int A[], int target, int lower, int upper) {
       int res = -1;
       while (lower <= upper) {
