0a121964b2de0e63702532fe493f978249b7ff7c
jfantasy <jfantasy90@gmail.com>
2014-08-28 18:23:15 +0800

    update 3sum_closest more simple

--- 3sum_closest.cc
+++ 3sum_closest.cc
@@ -4,6 +4,26 @@ class Solution {
       sort(num.begin(), num.end());
       int res = num[0] + num[1] + num[2];
       for (int i = 0; i < num.size(); ++ i) {
+        int l = i + 1, r = num.size() - 1;
+        while (l < r) {
+          int sum = num[i] + num[l] + num[r];
+          if (abs(sum - target) < abs(res - target)) res = sum;
+          if (sum < target) ++ l;
+          else -- r;
+        }
+      }
+      return res;
+    }
+};
+
+//------------
+
+class Solution {
+  public:
+    int threeSumClosest(vector<int> &num, int target) {
+      sort(num.begin(), num.end());
+      int res = num[0] + num[1] + num[2];
+      for (int i = 0; i < num.size(); ++ i) {
         for (int l = i + 1, r = num.size() - 1; l < r; ++ l) {
           while (l < r && num[i] + num[l] + num[r] > target) {
             if (abs(res - target) > abs(num[i] + num[l] + num[r] - target)) res = num[i] + num[l] + num[r];
