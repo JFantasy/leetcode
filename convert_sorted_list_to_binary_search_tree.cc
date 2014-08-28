dc4ce341f2a462077d34dfc9c7d34c0d71c66864
jfantasy <jfantasy90@gmail.com>
2014-08-28 13:38:27 +0800

    update remove_duplicates_from_sorted_array_ii more simple

--- convert_sorted_list_to_binary_search_tree.cc
+++ convert_sorted_list_to_binary_search_tree.cc
@@ -39,3 +39,42 @@ class Solution {
       return res;
     }
 };
+
+//-----------------------
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+  public:
+    TreeNode *build(ListNode *low, ListNode *upp) {
+      if (!low || low == upp) return NULL;
+      int length = 0;
+      ListNode *current;
+      for (current = low; current != upp; current = current->next) ++ length;
+      int mid = length >> 1;
+      current = low;
+      for (int i = 0; i < mid; ++ i, current = current->next);
+      TreeNode *res = new TreeNode(current->val);
+      res->left = build(low, current);
+      res->right = build(current->next, upp);
+      return res;
+    }
+    TreeNode *sortedListToBST(ListNode *head) {
+      return build(head, NULL);
+    }
+};
--- remove_duplicates_from_sorted_array_ii.cc
+++ remove_duplicates_from_sorted_array_ii.cc
@@ -1,6 +1,20 @@
 class Solution {
   public:
     int removeDuplicates(int A[], int n) {
+      if (n <= 2) return n;
+      int res = 2;
+      for (int i = 2; i < n; ++ i) {
+        if (A[i] != A[res - 2]) A[res ++] = A[i];
+      }
+      return res;
+    }
+};
+
+//---------------------------------
+
+class Solution {
+  public:
+    int removeDuplicates(int A[], int n) {
       int l, r, k;
       for (l = 0, r = 0, k = 0; r < n; ++ r) {
         if (l > 0 && A[l - 1] == A[r] && k == 2) continue;
