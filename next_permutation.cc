1018fdc6480e3c62a0e78af3336ab0a2c82d8a34
jfantasy <jfantasy90@gmail.com>
2014-08-29 09:57:05 +0800

    update next_permutation from O(nlogn) to O(n)

--- next_permutation.cc
+++ next_permutation.cc
@@ -5,6 +5,24 @@ class Solution {
       for (i = num.size() - 2; i >= 0; -- i) {
         if (num[i] < num[i + 1]) break;
       }
+      if (i < 0) reverse(num.begin(), num.end());
+      else {
+        for (j = i; j + 1 < num.size() && num[j + 1] > num[i]; ++ j);
+        swap(num[i], num[j]);
+        reverse(num.begin() + i + 1, num.end());
+      }
+    }
+};
+
+//---------
+
+class Solution {
+  public:
+    void nextPermutation(vector<int> &num) {
+      int i, j;
+      for (i = num.size() - 2; i >= 0; -- i) {
+        if (num[i] < num[i + 1]) break;
+      }
       if (i < 0) sort(num.begin(), num.end());
       else {
         for (j = i + 1; j < num.size() && num[j] > num[i]; ++ j);
