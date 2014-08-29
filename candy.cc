a892716ced8158437131f30819d4776de402e61b
jfantasy <jfantasy90@gmail.com>
2014-08-29 12:37:03 +0800

    update candy & single_number more simple

--- candy.cc
+++ candy.cc
@@ -1,15 +1,14 @@
 class Solution {
   public:
     int candy(vector<int> &ratings) {
-      vector<int> arr(ratings.size(), 1);
-      for (int i = 1; i < arr.size(); ++ i) {
+      const int n = ratings.size();
+      vector<int> arr(n, 1);
+      for (int i = 1; i < n; ++ i) {
         if (ratings[i] > ratings[i - 1]) arr[i] = arr[i - 1] + 1;
       }
-      for (int i = arr.size() - 2; i >= 0; -- i) {
+      for (int i = n - 2; i >= 0; -- i) {
         if (ratings[i] > ratings[i + 1]) arr[i] = max(arr[i + 1] + 1, arr[i]);
       }
-      int res = 0;
-      for (int i = 0; i < arr.size(); ++ i) res += arr[i];
-      return res;
+      return accumulate(arr.begin(), arr.end(), 0);
     }
 };
--- single_number.cc
+++ single_number.cc
@@ -1,6 +1,15 @@
 class Solution {
   public:
     int singleNumber(int A[], int n) {
+      return acuumulate(A, A + n, 0, bit_xor<int>());
+    }
+};
+
+//---------
+
+class Solution {
+  public:
+    int singleNumber(int A[], int n) {
       int res = 0;
       for (int i = 0; i < n; ++ i) res ^= A[i];
       return res;
