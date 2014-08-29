84a9836601dc2dbad4d5c5489d4344ecff5bfd73
jfantasy <jfantasy90@gmail.com>
2014-08-29 12:02:12 +0800

    update rotate_image & plus_one another solution

--- plus_one.cc
+++ plus_one.cc
@@ -1,6 +1,22 @@
 class Solution {
   public:
     vector<int> plusOne(vector<int> &digits) {
+      int n = digits.size() - 1, add = 1;
+      while (n >= 0) {
+        digits[n] += add;
+        add = digits[n] / 10;
+        digits[n --] %= 10;
+      }
+      if (add > 0) digits.insert(digits.begin(), 1);
+      return digits;
+    }
+};
+
+//---------
+
+class Solution {
+  public:
+    vector<int> plusOne(vector<int> &digits) {
       vector<int> res(digits);
       reverse(res.begin(), res.end());
       ++ res[0];
--- rotate_image.cc
+++ rotate_image.cc
@@ -1,6 +1,25 @@
 class Solution {
   public:
     void rotate(vector<vector<int> > &matrix) {
+      const int n = matrix.size();
+      for (int i = 0; i < n; ++ i) {
+        for (int j = 0; j < n - i; ++ j) {
+          swap(matrix[i][j], matrix[n - j - 1][n - i - 1]);
+        }
+      }
+      for (int i = 0; i < n / 2; ++ i) {
+        for (int j = 0; j < n; ++ j) {
+          swap(matrix[i][j], matrix[n - i - 1][j]);
+        }
+      }
+    }
+};
+
+//---------
+
+class Solution {
+  public:
+    void rotate(vector<vector<int> > &matrix) {
       for (int i = 0; i < matrix.size(); ++ i) {
         for (int j = i; j < matrix[i].size() - i - 1; ++ j) {
           int x = i, y = j;
