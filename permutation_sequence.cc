ad164eec886115dd1e0dbbfa739e1199afd8d21a
jfantasy <jfantasy90@gmail.com>
2014-08-29 10:14:18 +0800

    update permutation_sequence another solution

--- permutation_sequence.cc
+++ permutation_sequence.cc
@@ -1,6 +1,25 @@
 class Solution {
   public:
     string getPermutation(int n, int k) {
+      string num(n, '0'), res;
+      int base = 1;
+      -- k;
+      for (int i = 0; i < n; ++ i) num[i] += i + 1;
+      for (int i = 2; i < n; ++ i) base *= i;
+      for (int i = n - 1; i > 0; k %= base, base /= i, -- i) {
+        auto iter = next(num.begin(), k / base);
+        res.push_back(*iter);
+        num.erase(iter);
+      }
+      return res + *num.begin();
+    }
+};
+
+//---------
+
+class Solution {
+  public:
+    string getPermutation(int n, int k) {
       vector<int> fact(n, 1), use(n, 0);
       string res = "";
       -- k;
