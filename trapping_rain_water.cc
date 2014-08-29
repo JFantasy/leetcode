85677e07fa3351e06772765f0fadfa99dbc5035a
jfantasy <jfantasy90@gmail.com>
2014-08-29 10:26:54 +0800

    update trapping_rain_water more simple

--- trapping_rain_water.cc
+++ trapping_rain_water.cc
@@ -1,6 +1,22 @@
 class Solution {
   public:
     int trap(int A[], int n) {
+      vector<int> left(n, 0), right(n, 0);
+      for (int i = 1; i < n; ++ i) {
+        left[i] = max(left[i - 1], A[i - 1]);
+        right[n - i - 1] = max(right[n - i], A[n - i]);
+      }
+      int res = 0;
+      for (int i = 0; i < n; ++ i) res += max(0, min(left[i], right[i]) - A[i]);
+      return res;
+    }
+};
+
+//---------
+
+class Solution {
+  public:
+    int trap(int A[], int n) {
       vector<int> ls(n, 0), rs(n, 0);
       for (int i = 0; i < n; ++ i) {
         ls[i] = !i ? 0 : max(ls[i - 1], A[i - 1]);
