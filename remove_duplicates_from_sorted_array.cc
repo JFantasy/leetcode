ae9cc528ce20b7edeab4981ad10fe4278995be77
jfantasy <jfantasy90@gmail.com>
2014-08-29 09:11:45 +0800

    update remove_duplicates_from_sorted_array another solutions

--- remove_duplicates_from_sorted_array.cc
+++ remove_duplicates_from_sorted_array.cc
@@ -1,6 +1,29 @@
 class Solution {
   public:
     int removeDuplicates(int A[], int n) {
+      return distance(A, unique(A, A + n));
+    }
+};
+
+//---------
+
+class Solution {
+  public:
+    int removeDuplicates(int A[], int n) {
+      if (n == 0) return 0;
+      int l, r;
+      for (l = 1, r = 1; r < n; ++ r) {
+        if (A[r] != A[l - 1]) A[l ++] = A[r];
+      }
+      return l;
+    }
+};
+
+//---------
+
+class Solution {
+  public:
+    int removeDuplicates(int A[], int n) {
       int l, r;
       for (l = 0, r = 0; r < n; ++ r) {
         if (r == 0 || A[r] != A[r - 1]) A[l ++] = A[r];
