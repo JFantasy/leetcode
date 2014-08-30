f13af57896f45b599be6ed48b91da60c5ffa7b01
jfantasy <jfantasy90@gmail.com>
2014-08-30 17:04:56 +0800

    update swap_nodes using **

--- swap_nodes_in_k_group.cc
+++ swap_nodes_in_k_group.cc
@@ -9,6 +9,38 @@
 class Solution {
   public:
     ListNode *reverseKGroup(ListNode *head, int k) {
+      ListNode **p = &head;
+      while (*p) {
+        int count = 0;
+        for (ListNode *cur = *p; cur && count < k; ++ count) cur = cur->next;
+        if (count < k) break;
+        ListNode *cur = *p, *begin = *p;
+        for (int i = 0; i < k; ++ i) {
+          ListNode *next = cur->next;
+          cur->next = *p;
+          *p = cur;
+          cur = next;
+        }
+        begin->next = cur;
+        p = &(begin->next);
+      }
+      return head;
+    }
+};
+
+//---------
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+  public:
+    ListNode *reverseKGroup(ListNode *head, int k) {
       if (head == NULL) return NULL;
       if (k == 1) return head;
       int len;
--- swap_nodes_in_pairs.cc
+++ swap_nodes_in_pairs.cc
@@ -8,6 +8,32 @@
  */
 class Solution {
   public:
+    ListNode *swapPairs(ListNode *head) {
+      ListNode **p = &head;
+      while (*p) {
+        if ((*p)->next == nullptr) break;
+        ListNode *left = *p, *right = (*p)->next;
+        left->next = right->next;
+        right->next = left;
+        *p = right;
+        p = &(left->next);
+      }
+      return head;
+    }
+};
+
+//---------
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+  public:
     ListNode *swapList(ListNode *current) {
       if (current == NULL) return NULL;
       if (current->next == NULL) return current;
