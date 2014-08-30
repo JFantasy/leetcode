fd04f1c42967bf28f89b85b3f2979f4afb8676d6
jfantasy <jfantasy90@gmail.com>
2014-08-30 15:42:43 +0800

    update remove_duplicates_from_sorted_list_ii using **

--- remove_duplicates_from_sorted_list_ii.cc
+++ remove_duplicates_from_sorted_list_ii.cc
@@ -9,6 +9,65 @@
 class Solution {
   public:
     ListNode *deleteDuplicates(ListNode *head) {
+      ListNode **p = &head;
+      bool dup = false;
+      while (*p) {
+        if ((*p)->next != nullptr && (*p)->val == (*p)->next->val || dup) {
+          dup = (*p)->next != nullptr && (*p)->val == (*p)->next->val;
+          ListNode *current = *p;
+          *p = (*p)->next;
+          delete current;
+        } else p = &((*p)->next);
+      }
+      return head;
+    }
+};
+
+//---------
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+  public:
+    ListNode *deleteDuplicates(ListNode *head) {
+      ListNode **p = &head;
+      while (*p) {
+        bool dup = false;
+        while ((*p)->next != nullptr && (*p)->next->val == (*p)->val) {
+          dup = true;
+          ListNode *current = *p;
+          *p = current->next;
+          delete current;
+        }
+        if (dup) {
+          ListNode *current = *p;
+          *p = current->next;
+          delete current;
+        } else p = &((*p)->next);
+      }
+      return head;
+    }
+};
+
+//---------
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+  public:
+    ListNode *deleteDuplicates(ListNode *head) {
       if (!head) return NULL;
       if (!head->next) return head;
       if (head->val == head->next->val) {
