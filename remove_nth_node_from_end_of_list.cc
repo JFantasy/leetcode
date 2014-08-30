3421915dd3bfa10b67652adec471cd2f020cc9ef
jfantasy <jfantasy90@gmail.com>
2014-08-30 16:17:01 +0800

    update remove_nth_node_from_end_of_list using **

--- remove_nth_node_from_end_of_list.cc
+++ remove_nth_node_from_end_of_list.cc
@@ -9,6 +9,33 @@
 class Solution {
   public:
     ListNode *removeNthFromEnd(ListNode *head, int n) {
+      ListNode **p = &head;
+      ListNode *tail = head;
+      while (n --) tail = tail->next;
+      while (tail) {
+        p = &(*p)->next;
+        tail = tail->next;
+      }
+      ListNode *target = *p;
+      *p = (*p)->next;
+      delete target;
+      return head;
+    }
+};
+
+//---------
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+  public:
+    ListNode *removeNthFromEnd(ListNode *head, int n) {
       ListNode *l = head, *r = head, *res = head;
       while (n --) r = r->next;
       if (r == NULL) {
