7910829a9587abe63a6fb511befbe5f7b1b16986
jfantasy <jfantasy90@gmail.com>
2014-08-30 18:25:47 +0800

    update merge_two_sorted_lists using **

--- merge_two_sorted_lists.cc
+++ merge_two_sorted_lists.cc
@@ -9,6 +9,37 @@
 class Solution {
   public:
     ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+      ListNode *head = NULL;
+      ListNode **p = &head;
+      while (l1 != nullptr && l2 != nullptr) {
+        if (l1->val < l2->val) {
+          *p = l1;
+          l1 = l1->next;
+        } else {
+          *p = l2;
+          l2 = l2->next;
+        }
+        p = &((*p)->next);
+      }
+      if (l1) *p = l1;
+      if (l2) *p = l2;
+      return head;
+    }
+};
+
+//---------
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+  public:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
       if (!l1 && !l2) return NULL;
       ListNode *res = l1;
       if (!l1 || (l2 && l1->val > l2->val)) res = l2;
