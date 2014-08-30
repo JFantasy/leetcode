f6e36951d0dab58286e9539fbc6eda19de6d079b
jfantasy <jfantasy90@gmail.com>
2014-08-30 18:26:03 +0800

    update insertion_sort_list using **

--- insertion_sort_list.cc
+++ insertion_sort_list.cc
@@ -10,6 +10,32 @@ class Solution {
   public:
     ListNode *insertionSortList(ListNode *head) {
       ListNode *res = NULL;
+      while (head) {
+        ListNode **p = &res;
+        ListNode *next = head->next;
+        while (*p && (*p)->val < head->val) p = &((*p)->next);
+        head->next = *p;
+        *p = head;
+        head = next;
+      }
+      return res;
+    }
+};
+
+//---------
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+  public:
+    ListNode *insertionSortList(ListNode *head) {
+      ListNode *res = NULL;
       while (true) {
         if (!head) break;
         ListNode *next = head->next;
