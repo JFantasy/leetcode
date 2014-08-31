42f529cf94235d529228488c19d556504e93b8a8
jfantasy <jfantasy90@gmail.com>
2014-08-31 10:13:09 +0800

    update copy_list_with_random_pointer from O(n) space to O(1) space

--- copy_list_with_random_pointer.cc
+++ copy_list_with_random_pointer.cc
@@ -9,6 +9,39 @@
 class Solution {
   public:
     RandomListNode *copyRandomList(RandomListNode *head) {
+      for (RandomListNode *current = head; current != nullptr; current = current->next) {
+        RandomListNode *node = new RandomListNode(current->label);
+        node->next = current->next;
+        current->next = node;
+        current = node;
+      }
+      for (RandomListNode *current = head; current != nullptr; current = current->next->next) {
+        if (current->random == nullptr) continue;
+        current->next->random = current->random->next;
+      }
+      RandomListNode *res = NULL, **p = &res;
+      for (RandomListNode *current = head; current != nullptr; current = current->next) {
+        *p = current->next;
+        current->next = current->next->next;
+        p = &((*p)->next);
+      }
+      return res;
+    }
+};
+
+//---------
+
+/**
+ * Definition for singly-linked list with a random pointer.
+ * struct RandomListNode {
+ *     int label;
+ *     RandomListNode *next, *random;
+ *     RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+ * };
+ */
+class Solution {
+  public:
+    RandomListNode *copyRandomList(RandomListNode *head) {
       if (!head) return NULL;
       map<RandomListNode *, RandomListNode *> dict;
       queue<RandomListNode *> que;
