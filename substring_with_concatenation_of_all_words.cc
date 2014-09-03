640affe7424a56b114ea23e68d9564bda2624361
jfantasy <jfantasy90@gmail.com>
2014-09-03 15:59:25 +0800

    update substring_with_concatenation_of_all_words another solution with better time cost

--- substring_with_concatenation_of_all_words.cc
+++ substring_with_concatenation_of_all_words.cc
@@ -5,6 +5,45 @@ class Solution {
       for (int i = 0; i < L.size(); ++ i) ++ dict[L[i]];
       vector<int> res;
       int len = L[0].length();
+      for (int start = 0; start < len; ++ start) {
+        unordered_map<string, int> current;
+        int l = start, r = l, reach = 0;
+        while (true) {
+          if (r + len > S.length()) break;
+          string word = S.substr(r, len);
+          r += len;
+          if (dict.find(word) == dict.end()) {
+            reach = 0;
+            current.clear();
+            l = r;
+          } else {
+            ++ current[word];
+            if (current[word] == dict[word]) ++ reach;
+            else {
+              while (current[word] > dict[word]) {
+                string rword = S.substr(l, len);
+                if (current[rword] == dict[rword]) -- reach;
+                -- current[rword];
+                l += len;
+              }
+            }
+            if (reach == dict.size()) res.push_back(l);
+          }
+        }
+      }
+      return res;
+    }
+};
+
+//---------
+
+class Solution {
+  public:
+    vector<int> findSubstring(string S, vector<string> &L) {
+      unordered_map<string, int> dict;
+      for (int i = 0; i < L.size(); ++ i) ++ dict[L[i]];
+      vector<int> res;
+      int len = L[0].length();
       for (int i = 0; i + L.size() * len <= S.length(); ++ i) {
         unordered_map<string, int> current;
         bool flag = true;
