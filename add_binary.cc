17a77ee8870a1f0931058b74c319a3301f9d7ae8
jfantasy <jfantasy90@gmail.com>
2014-09-06 10:41:21 +0800

    update add_binary

--- add_binary.cc
+++ add_binary.cc
@@ -1,6 +1,25 @@
 class Solution {
   public:
     string addBinary(string a, string b) {
+      string res;
+      int n = a.length(), m = b.length(), len = max(n, m), add = 0;
+      for (int i = 0; i < len; ++ i) {
+        int l = i < n ? a[n - i - 1] - '0' : 0;
+        int r = i < m ? b[m - i - 1] - '0' : 0;
+        int x = (l + r + add) & 1, y = (l + r + add) >> 1;
+        res.insert(res.begin(), '0' + x);
+        add = y;
+      }
+      if (add) res.insert(res.begin(), '1');
+      return res;
+    }
+};
+
+//---------
+
+class Solution {
+  public:
+    string addBinary(string a, string b) {
       reverse(a.begin(), a.end());
       reverse(b.begin(), b.end());
       string res(max(a.length(), b.length()), 0);
