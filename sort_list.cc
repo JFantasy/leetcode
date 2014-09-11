a9c9f72b85380d89bedcb17465e2a67ae1c7fc0d
jfantasy <275428266@qq.com>
2014-09-11 14:31:43 +0800

    update sort_list from O(logN) space to O(1) space using **

--- sort_list.cc
+++ sort_list.cc
@@ -7,6 +7,52 @@
  * };
  */
 class Solution {
+public:
+    ListNode *sortList(ListNode *head) {
+        if (head == nullptr) return head;
+        ListNode **phd = &head, **ptl = &head;
+        while (*ptl != nullptr) ptl = &((*ptl)->next);
+        while (true) {
+            ListNode *left = head, *right, *start = head, *end;
+            while (left->next != nullptr && left->val <= left->next->val) left = left->next;
+            if (left->next == nullptr) break;
+            right = left->next;
+            while (right->next != nullptr && right->val <= right->next->val) right = right->next;
+            if (right->next != nullptr) *phd = right->next;
+            else ptl = &head;
+            end = right;
+            right = left->next;
+            left->next = end->next = nullptr;
+            left = start;
+            while (left != nullptr && right != nullptr) {
+                if (left->val < right->val) {
+                    *ptl = left;
+                    left = left->next;
+                } else {
+                    *ptl = right;
+                    right = right->next;
+                }
+                ptl = &((*ptl)->next);
+            }
+            if (left != nullptr) *ptl = left;
+            if (right != nullptr) *ptl = right;
+            while (*ptl != nullptr) ptl = &((*ptl)->next);
+        }
+        return head;
+    }
+};
+
+//---------
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
   public:
     int getLength(ListNode *head) {
       int res = 0;
