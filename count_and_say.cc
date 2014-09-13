e8d8c97acc71d18c13573648b7050c8981b38f5c
jfantasy <jfantasy90@gmail.com>
2014-09-13 11:48:38 +0800

    update count_and_say

--- count_and_say.cc
+++ count_and_say.cc
@@ -1,6 +1,29 @@
 class Solution {
   public:
     string getNext(const string &str) {
+      stringstream ss;
+      for (int i = 0; i < str.length(); ++i) {
+        int j = i;
+        while (j + 1 < str.length() && str[i] == str[j + 1]) ++j;
+        ss << j - i + 1 << str[i];
+        i = j;
+      }
+      return ss.str();
+    }
+    string countAndSay(int n) {
+      string res = "1";
+      while (--n) {
+        res = getNext(res);
+      }
+      return res;
+    }
+};
+
+//---------
+
+class Solution {
+  public:
+    string getNext(const string &str) {
       string res = "";
       for (int i = 0; i < str.length(); ++ i) {
         int j;
