38d15e17d16bddc0bc008616b2b73312a4060290
jfantasy <jfantasy90@gmail.com>
2014-09-15 11:12:26 +0800

    update binary_tree_preorder_traversal another solution

--- binary_tree_preorder_traversal.cc
+++ binary_tree_preorder_traversal.cc
@@ -8,6 +8,35 @@
  * };
  */
 class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode *root) {
+        stack<TreeNode *> stk;
+        vector<int> res;
+        stk.push(root);
+        while (!stk.empty()) {
+            TreeNode *current = stk.top();
+            stk.pop();
+            if (current == nullptr) continue;
+            res.push_back(current->val);
+            stk.push(current->right);
+            stk.push(current->left);
+        }
+        return res;
+    }
+};
+
+//---------
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
   public:
     void dfs(TreeNode *root, vector<int> &res) {
       if (!root) return;
