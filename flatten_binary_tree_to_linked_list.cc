a8942b55aa56d6c71d1b08630f752ef41ca817a1
jfantasy <jfantasy90@gmail.com>
2014-09-15 15:23:09 +0800

    update flatten_binary_tree_to_linked_list another solution

--- flatten_binary_tree_to_linked_list.cc
+++ flatten_binary_tree_to_linked_list.cc
@@ -1,3 +1,32 @@
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode *root) {
+        if (root == nullptr) return;
+        stack<TreeNode *> stk;
+        stk.push(root);
+        while (!stk.empty()) {
+            TreeNode *current = stk.top();
+            stk.pop();
+            if (current->right != nullptr) stk.push(current->right);
+            if (current->left != nullptr) stk.push(current->left);
+            current->left = nullptr;
+            if (!stk.empty()) current->right = stk.top();
+        }
+    }
+};
+
+//---------
+
 /**
  * Definition for binary tree
  * struct TreeNode {
