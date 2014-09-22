f9e72d4ccbf557ce23ddd0962c8f9a862e1b5665
jfantasy <jfantasy90@gmail.com>
2014-09-22 14:23:38 +0800

    update decode_ways & merge_sorted_array a little fix

--- decode_ways.cc
+++ decode_ways.cc
@@ -1,14 +1,14 @@
 class Solution {
   public:
-    int numDecodings(string s) {
-      if (s.length() < 1) return 0;
+    int numDecodings(string s) const {
+      if (s.empty()) return 0;
       vector<int> f(s.length() + 1);
       f[0] = 1;
       f[1] = (s[0] > '0');
-      for (int i = 2; i <= s.length(); ++ i) {
+      for (int i = 2; i < f.length(); ++ i) {  // i < f.length()
         int val = (s[i - 2] - '0') * 10 + (s[i - 1] - '0');
         f[i] = f[i - 1] * (s[i - 1] > '0') + f[i - 2] * (val >= 10 && val <= 26);
       }
-      return f[s.length()];
+      return f[f.length() - 1];
     }
 };
--- merge_sorted_array.cc
+++ merge_sorted_array.cc
@@ -1,4 +1,18 @@
 class Solution {
+public:
+    void merge(int A[], int m, int B[], int n) {
+        int i = m - 1, j = n - 1, current = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            if (A[i] > B[j]) A[current--] = A[i--];
+            else A[current--] = B[j--]; 
+        }
+        while (j >= 0) A[current--] = B[j--];
+    }
+};
+
+//---------
+
+class Solution {
   public:
     void merge(int A[], int m, int B[], int n) {
       for (int ap = m - 1, bp = n - 1, index = m + n - 1; index >= 0; -- index) {
