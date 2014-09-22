93d99cc900f47977936db3ad7121a19ace4923ab
jfantasy <jfantasy90@gmail.com>
2014-09-22 14:30:37 +0800

    update search_for_a_range more simple using STL

--- search_for_a_range.cc
+++ search_for_a_range.cc
@@ -1,5 +1,16 @@
 class Solution {
   public:
+    vector<int> searchRange(int A[], int n, int target) {
+      int l = distance(A, lower_bound(A, A + n, target));
+      int r = distance(A, upper_bound(A, A + n, target) - 1);
+      return l == n || A[l] != target ? vector<int>({-1, -1}) : vector<int>({l, r});
+    }
+};
+
+//----------
+
+class Solution {
+  public:
     int find(int A[], int n, int target, int flag) {
       int low = 0, upp = n - 1, res = -1;
       while (low <= upp) {
