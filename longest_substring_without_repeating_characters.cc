4efb86f9198a3daf437eba8e1cc750563cd3206b
jfantasy <jfantasy90@gmail.com>
2014-09-24 21:23:21 +0800

    update longest_substring_without_repeating_characters another solution

--- longest_substring_without_repeating_characters.cc
+++ longest_substring_without_repeating_characters.cc
@@ -1,6 +1,27 @@
 class Solution {
   public:
     int lengthOfLongestSubstring(string s) {
+      const int kAsciiMax = 256;
+      const int n = s.length();
+      vector<int> last(kAsciiMax, -1);
+      int res = 0, start = 0;
+      for (int i = 0; i < n; ++i) {
+        if (last[int(s[i])] >= start) {
+          res = max(res, i - start);
+          start = last[int(s[i])] + 1;
+        }
+        last[int(s[i])] = i;
+      }
+      res = max(res, n - start);
+      return res;
+    }
+};
+
+//---------
+
+class Solution {
+  public:
+    int lengthOfLongestSubstring(string s) {
       map<char, int> count;
       int res = 0;
       for (int l = 0, r = 0; r < s.length(); ++ r) {
