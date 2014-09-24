ad81ccfd4430be7f207242f9d72858115737963f
jfantasy <jfantasy90@gmail.com>
2014-09-24 14:04:23 +0800

    a little fix

--- maximum_product_subarray.cc
+++ maximum_product_subarray.cc
@@ -3,15 +3,14 @@ class Solution {
     int maxProduct(int A[], int n) {
       int res = A[0];
       for (int i = 1; i < n; ++i) res = max(res, A[i]);
-      for (int i = 0, negative = 0, op = 0, current = 1; i < n; ++i) {
+      for (int i = 0, negative = 0, current = 1; i < n; ++i) {
         if (A[i] == 0) {
-          negative = op = 0;
+          negative = 0;
           current = 1;
         } else {
-          op ^= (A[i] < 0);
-          current *= abs(A[i]);
-          if (op & 1) {
-            if (negative == 0) negative = abs(current);
+          current *= A[i];
+          if (current < 0) {
+            if (negative == 0) negative = current;
             else res = max(res, current / negative);
           } else res = max(res, current);
         }
