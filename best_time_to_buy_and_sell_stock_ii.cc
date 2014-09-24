c133e7c06758445ad96eabc09b78bc3adfe4ce33
jfantasy <jfantasy90@gmail.com>
2014-09-24 10:59:59 +0800

    update best_time_to_buy_and_sell_stock_ii another solution

--- best_time_to_buy_and_sell_stock_ii.cc
+++ best_time_to_buy_and_sell_stock_ii.cc
@@ -1,6 +1,17 @@
 class Solution {
   public:
     int maxProfit(vector<int> &prices) {
+      int res = 0;
+      for (int i = 1; i < prices.size(); ++i) res += max(0, prices[i] - prices[i - 1]);
+      return res;
+    }
+};
+
+//---------
+
+class Solution {
+  public:
+    int maxProfit(vector<int> &prices) {
       if (prices.size() <= 1) return 0;
       vector<int> f(prices.size(), 0);
       int res = 0;
