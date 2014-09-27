430799bd1e2ef2045ed968439429fd6ed39e1ea8
jfantasy <jfantasy90@gmail.com>
2014-09-27 11:56:04 +0800

    update palindrome_number fix overflow bug

--- palindrome_number.cc
+++ palindrome_number.cc
@@ -2,6 +2,24 @@ class Solution {
   public:
     bool isPalindrome(int x) {
       if (x < 0) return false;
+      int power = 1;
+      while (power <= x / 10) power *= 10;
+      while (x > 0) {
+        int i = x / power, j = x % 10;
+        if (i != j) return false;
+        x = x % power / 10;
+        power /= 100;
+      }
+      return true;
+    }
+};
+
+//---------
+
+class Solution {
+  public:
+    bool isPalindrome(int x) {
+      if (x < 0) return false;
       int val = x, y = 0, bak = val;
       for ( ; val; val /= 10) y = y * 10 + val % 10;
       return bak == y;
