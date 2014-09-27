1811baa078b89e736d6263bb3663dd49867a67b4
jfantasy <jfantasy90@gmail.com>
2014-09-27 11:39:04 +0800

    update word_break another solution

--- word_break_ii.cc
+++ word_break_ii.cc
@@ -1,5 +1,49 @@
 class Solution {
   public:
+    void getPrev(const int &length, const string &s, const unordered_set<string> &dict, vector<int> &dp, vector<vector<int> > &pre) {
+      dp[0] = 1;
+      for (int i = 0; i < length; ++i) {
+        if (dp[i] == 0) continue;
+        string current = "";
+        for (int j = i; j < length; ++j) {
+          current += s[j];
+          if (dict.find(current) != dict.end()) dp[j + 1] = pre[j + 1][i] = 1;
+        }
+      }
+    }
+    void genPath(const int &current, const string &s, const vector<vector<int> > &pre, vector<string> &res, vector<string> &path) {
+      if (current == 0) {
+        string line = "";
+        for (auto iter = path.rbegin(); iter != path.rend(); ++iter) {
+          line += *iter + " ";
+        }
+        line.pop_back();
+        res.push_back(line);
+      } else {
+        for (int i = 0; i < current; ++i) {
+          if (pre[current][i] > 0) {
+            path.push_back(s.substr(i, current - i));
+            genPath(i, s, pre, res, path);
+            path.pop_back();
+          }
+        }
+      }
+    }
+    vector<string> wordBreak(string s, unordered_set<string> &dict) {
+      const int n = s.length();
+      vector<int> dp(n + 1, 0);
+      vector<vector<int> > pre(n + 1, vector<int>(n + 1, 0));
+      getPrev(n, s, dict, dp, pre);
+      vector<string> res, path;
+      genPath(n, s, pre, res, path);
+      return res;
+    }
+};
+
+//---------
+
+class Solution {
+  public:
     vector<string> dfs(int start, string &str, map<int, vector<string> > &ans, unordered_set<string> &dict) {
       if (ans.find(start) != ans.end()) return ans[start];
       vector<string> res;
