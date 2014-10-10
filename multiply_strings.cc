f14781d0abdb4269240a82c0b7248745b17354bb
jfantasy <jfantasy90@gmail.com>
2014-10-10 21:39:58 +0800

    update multiply_strings

--- multiply_strings.cc
+++ multiply_strings.cc
@@ -1,5 +1,34 @@
 class Solution {
   public:
+    string multiply(string num1, string num2) {
+      int op = *num1.begin() == '-' ^ *num2.begin() == '-';
+      if (*num1.begin() == '-') num1 = num1.substr(1);
+      if (*num2.begin() == '-') num2 = num2.substr(1);
+      reverse(num1.begin(), num1.end());
+      reverse(num2.begin(), num2.end());
+      const int n = num1.length(), m = num2.length();
+      string res(n + m, 0);
+      for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+          res[i + j] += (num1[i] - '0') * (num2[j] - '0');
+          if (res[i + j] >= 10) {
+            res[i + j + 1] += res[i + j] / 10;
+            res[i + j] %= 10;
+          }
+        }
+      }
+      while (res.length() > 1 && *res.rbegin() == 0) res.pop_back();
+      for (auto &ch : res) ch += '0';
+      if (op > 0) res.push_back('-');
+      reverse(res.begin(), res.end());
+      return res;
+    }
+};
+
+//---------
+
+class Solution {
+  public:
     string process(string num1, string num2) {
       string res(num1.length() + num2.length(), '0');
       reverse(num1.begin(), num1.end());
